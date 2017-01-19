dce91aeb1a657a43bd2a0c73187f0a47e9c5b400
Zoltan Biber <zoltan.biber@gmail.com>
2017-01-19 14:22:42 +0100

    Strikethrough tasks that are ticked done.

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Row } from 'antd';
+import { Row, Col, Checkbox } from 'antd';
 
 const TodoItemWrapper = styled.div`
   background-color: dodgerblue;
@@ -10,12 +10,29 @@ const TodoItemWrapper = styled.div`
   padding: 0.5em;
 `;
 
-const TodoItem = ({itemContent}) => {
-  return (
-    <Row>
-      <TodoItemWrapper>{itemContent}</TodoItemWrapper>
-    </Row>
-  )
+const StrikethroughText = styled.p`text-decoration: line-through;`
+
+class TodoItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { done: false, strikethrough: false };
+    this.toggleDoneStatus = this.toggleDoneStatus.bind(this);
+  }
+
+  toggleDoneStatus(e) {
+    this.setState({ done: e.target.checked, strikethrough: e.target.checked });
+  };
+
+  render() {
+    return (
+      <TodoItemWrapper>
+        <Row>
+          <Col span={22}>{this.state.strikethrough ? <StrikethroughText>{this.props.itemContent}</StrikethroughText> : this.props.itemContent}</Col>
+          <Col span={2}><Checkbox onChange={this.toggleDoneStatus}/></Col>
+        </Row>
+      </TodoItemWrapper>
+    )
+  }
 };
 
 export default TodoItem;
