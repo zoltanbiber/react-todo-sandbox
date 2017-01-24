83243500adcd97701d201a0cda0e181b3471f84d
Zoltan Biber <zoltan.biber@gmail.com>
2017-01-24 16:17:43 +0100

    Skeleton /about page works.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { Router, Route, Link, browserHistory } from 'react-router'
 import App from './App';
+import About from './modules/About';
 import store from './store';
 import './index.css';
 
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router history={browserHistory}>
+      <Route path="/" component={App}/>
+      <Route path="/about" component={About}/>
+    </Router>
   </Provider>,
   document.getElementById('root')
 );
